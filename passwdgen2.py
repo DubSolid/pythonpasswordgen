c63f970bc250a85c6aa9dd8b3d600e4cc3896262
Chris <71638505+antigone-hck@users.noreply.github.com>
2022-01-20 15:18:31 +0100

    Update passwdgen2.py

--- passwdgen2.py
+++ passwdgen2.py
@@ -1,40 +1,38 @@
 import random
 import string
-import tkinter as tk
-import sys
-import os.path
+from tkinter import *
 
 #GUI setup
-window = tk.Tk()
-window.geometry("500x300")
+window = Tk(className=' PassGen')
+window.geometry("400x300")
 
 #ascii chars definition
-length = 50
+length = 20
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 num = string.digits
 symbols = string.punctuation
 
-#password generation
+#password generation 
 def generate():
     all = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.sample(all,length))
     return password
 password = generate()
 
-#printing out password to both a file and GUI
+#printing out password to both a txt file and GUI
 def printpasswd():
     generated_password = generate()
-    outF = open("passwords.txt", "a")
+    outF = open("generated-passwords.txt", "a")
     outF.write(generated_password + "\n")
     outF.close()
-    label_var = tk.StringVar()
+    label_var = StringVar()
     label_var.set(generated_password)
-    my_label = tk.Label(master=window, width=100, height=1, textvar=label_var)
+    my_label = Label(master=window, width=100, height=1, textvar=label_var)
     my_label.pack()
 
 #button to generate password in GUI
-button = tk.Button(
+button = Button(
     master=window,
     text = "Generate password",
     width=15,
